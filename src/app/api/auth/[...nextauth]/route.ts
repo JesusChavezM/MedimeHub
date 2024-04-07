4453173398777a311bfe0a3137a06d5799959f7f
Jesus Chavez <chavezmele7030@gmail.com>
2024-04-07 16:31:38 -0600

    Fix build nextAuth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,67 +6,63 @@ import connect from "../../../../lib/mongodb";
 import bcrypt from "bcryptjs";
 import User from "../../../../models/user";
 
-
-export const authOptions: any = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-
-      },
-      async authorize(credentials: any) {
-        await connect();
-        try {
-          const user = await User.findOne({ email: credentials.email });
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+          id: "credentials",
+          name: "Credentials",
+          credentials: {
+            email: { label: "Email", type: "text" },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials: any) {
+            await connect();
+            try {
+              const user = await User.findOne({ email: credentials.email });
+              if (user) {
+                const isPasswordCorrect = await bcrypt.compare(
+                  credentials.password,
+                  user.password
+                );
+                if (isPasswordCorrect) {
+                  return user;
+                }
+              }
+            } catch (err: any) {
+              throw new Error(err);
             }
-          }
-        } catch (err: any) {
-          throw new Error(err);
-        }
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn({ user, account }: { user: AuthUser; account: Account }) {
-      if (account?.provider == "credentials") {
-        return true;
-      }
-      if (account?.provider == "google") {
-        await connect();
-        try {
-          const existingUser = await User.findOne({ email: user.email });
-          if (!existingUser) {
-            const newUser = new User({
-              email: user.email,
-            });
-
-            await newUser.save();
+          },
+        }),
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        }),
+      ],
+      secret: process.env.NEXTAUTH_SECRET,
+      callbacks: {
+        async signIn({ user, account }: { user: AuthUser; account: Account }) {
+          if (account?.provider == "credentials") {
             return true;
           }
-          return true;
-        } catch (err) {
-          console.log("Error saving user", err);
-          return false;
-        }
-      }
-    },
-  },
-};
-
-export const handler = NextAuth(authOptions);
+          if (account?.provider == "google") {
+            await connect();
+            try {
+              const existingUser = await User.findOne({ email: user.email });
+              if (!existingUser) {
+                const newUser = new User({
+                  email: user.email,
+                });
+    
+                await newUser.save();
+                return true;
+              }
+              return true;
+            } catch (err) {
+              console.log("Error saving user", err);
+              return false;
+            }
+          }
+        },
+      },
+});
 export { handler as GET, handler as POST };
