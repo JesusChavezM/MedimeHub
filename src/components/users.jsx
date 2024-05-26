c7e1721d6c72dcb5ff4bfb87fbd2f90751449e1f
chavez <chavezmele7030@gmail.com>
2024-05-25 23:00:00 -0600

    Update users component role admin managment

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,24 +1,23 @@
-"use client";
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 import ImgUser from "../assets/img_user.svg";
-import ImgDoctor from "../assets/img_doctor.svg";
 import ImgEdit from "../assets/img_edit.svg";
 import ImgDelete from "../assets/img_delete.svg";
 
 export default function UsersPage() {
+  const [admins, setAdmins] = useState([]);
+  const [doctors, setDoctors] = useState([]);
   const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [expandedOptions, setExpandedOptions] = useState({});
+
+  const getUsers = async () => {
+    const response = await fetch("/api/users", { cache: "no-store" });
+    const data = await response.json();
+    setAdmins(data.filter((user) => user.role === "admin"));
+    setDoctors(data.filter((user) => user.role === "doctor"));
+    setUsers(data.filter((user) => user.role === "user"));
+  };
 
   useEffect(() => {
-    const getUsers = async () => {
-      const response = await fetch("/api/users", { cache: "no-store" });
-      const data = await response.json();
-      setUsers(data);
-    };
-
     getUsers();
   }, []);
 
@@ -29,116 +28,95 @@ export default function UsersPage() {
     if (data.error) {
       console.error(data.error);
     } else {
-      setUsers(users.filter((user) => user._id !== id));
+      getUsers(); // Vuelve a cargar todas las tablas después de eliminar un usuario
     }
   };
 
+  const updateUserRole = async (userId, isAdmin) => {
+    const newRole = isAdmin ? "admin" : "user";
+    console.log("User ID:", userId);
+    console.log("New Role:", newRole);
+
+    try {
+      const response = await fetch(`/api/user?id=${encodeURIComponent(userId)}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ role: newRole }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to update user role. Status: ${response.status}`);
+      }
+
+      getUsers(); // Vuelve a cargar todas las tablas después de actualizar el rol de un usuario
+    } catch (error) {
+      console.error("Error updating user role:", error);
+    }
+  };
+
+  const renderTable = (title, data) => (
+    <div className="w-full mb-8">
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <table className="w-full border-collapse border border-800">
+        <thead>
+          <tr className="bg-purple-200">
+            <th className="border border-800">Foto</th>
+            <th className="border border-800 p-2">Nombre</th>
+            <th className="border border-800 p-2 hidden md:table-cell">Correo Electrónico</th>
+            <th className="border border-800 p-2">Rol</th>
+            <th className="border border-800 p-2">Admin</th>
+            <th className="border border-800 p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user._id} className="bg-100">
+              <td className="border border-800 p-px">
+                <div className="flex items-center justify-center">
+                  <img
+                    src={user.image || ImgUser}
+                    className="w-10 h-10 rounded-full"
+                  />
+                </div>
+              </td>
+              <td className="border border-800 p-2">{user.name}</td>
+              <td className="border border-800 p-2 hidden md:table-cell">{user.email}</td>
+              <td className="border border-800 p-2 hidden md:table-cell">{user.role}</td>
+              <td className="border border-800 p-2 text-center">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={user.role === "admin"}
+                    onChange={(e) => updateUserRole(user._id, e.target.checked)}
+                  />
+                  Admin
+                </label>
+              </td>
+              <td className="border border-800 p-2 space-x-2">
+                <button onClick={() => deleteUser(user._id)}>
+                  <Image src={ImgDelete} height={20} width={20} alt="delete-button" />
+                </button>
+                <button onClick={() => editUser(user._id)}>
+                  <Image src={ImgEdit} height={18} width={18} alt="edit-button" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+
   return (
     <div className="flex min-h-90vh flex-col items-center justify-between p-6">
-      <h1 className="flex items-center justify-center text-2xl font-bold mb-4 w-full">
-        Lista de Usuarios
-      </h1>
+      <h1 className="flex items-center justify-center text-2xl font-bold mb-4 w-full">Lista de Usuarios</h1>
       <div className="w-full">
         <div className="hidden sm:block">
-          <table className="w-full border-collapse border border-800">
-            <thead>
-              <tr className="bg-purple-200">
-                <th className="border border-800">Foto</th>
-                <th className="border border-800 p-2">Nombre</th>
-                <th className="border border-800 p-2 hidden md:table-cell">
-                  Correo Electrónico
-                </th>
-                <th className="border border-800 p-2 hidden md:table-cell">
-                  ID
-                </th>
-                <th className="border border-800 p-2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user._id} className="bg-100">
-                  <td className="border border-800 p-px">
-                    <div className="flex items-center justify-center">
-                      <img
-                        src={user.image || ImgUser}
-                        className="w-10 h-10 rounded-full"
-                      />
-                    </div>
-                  </td>
-                  <td className="border border-800 p-2">{user.name}</td>
-                  <td className="border border-800 p-2 hidden md:table-cell">
-                    {user.email}
-                  </td>
-                  <td className="border border-800 p-2 hidden md:table-cell">
-                    {user._id}
-                  </td>
-                  <td className="border border-800 p-2 space-x-2">
-                    <button onClick={() => deleteUser(user._id)}>
-                      <Image
-                        src={ImgDelete}
-                        height={20}
-                        width={20}
-                        alt="delete-button"
-                      />
-                    </button>
-                    <button onClick={() => editUser(user._id)}>
-                      <Image
-                        src={ImgEdit}
-                        height={18}
-                        width={18}
-                        alt="edit-button"
-                      />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="">
-          <div className="sm:hidden">
-            <div className="grid grid-cols-1 gap-3  w-[380px]">
-              {users.map((user) => (
-                <div
-                  key={user._id}
-                  className="bg-purple-200 border border-purple-800 rounded-lg shadow-md p-4 m-1 "
-                >
-                  <div className="flex items-center mb-2">
-                    <img
-                      src={user.image || ImgUser}
-                      alt={user.name}
-                      className="w-10 h-10 rounded-full mr-2"
-                    />
-                    <div>
-                      <p className="font-bold text-lg">{user.name}</p>
-                      <p className="text-gray-500 truncate">{user.email}</p>
-                      <p className="text-gray-500 truncate">Id: {user._id}</p>
-                      <p className="flex gap-2">
-                        {" "}
-                        Acciones:
-                        <button onClick={() => deleteUser(user._id)}>
-                          <Image
-                            src={ImgDelete}
-                            height={20}
-                            width={20}
-                            alt="delete-button"
-                          />
-                        </button>
-                        <button onClick={() => editUser(user._id)}>
-                          <Image
-                            src={ImgEdit}
-                            height={18}
-                            width={18}
-                            alt="edit-button"
-                          />
-                        </button>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+          {renderTable("Administradores", admins)}
+          {renderTable("Doctores", doctors)}
+          {renderTable("Usuarios", users)}
         </div>
       </div>
     </div>
