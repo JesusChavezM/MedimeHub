70a25cf1d7b5c2e8299ab3714f0d197897144363
Jesús Alberto Chavez Melero <chavezmele7030@gmail.com>
2024-05-30 13:13:43 -0600

    Update page.tsx commented qery integration

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -115,7 +115,7 @@ const Profile = () => {
                                     <Image src={ImgEdit} alt="Edit Profile" width={30} height={30} />
                                     Editar Perfil
                                 </button>
-                                {session.user.role === 'doctor' && (
+                           {/*     {session.user.role === 'doctor' && (
                                     <div className='flex flex-col justify-center items-center'>
                                         <button className="w-full justify-center border border-600 bg-white text-black p-2 rounded-2xl hover:bg-200 flex items-center space-x-6" onClick={() => router.push(`/edit-profile?email=${session.user.email}`)}>
                                             <Image src="/static/media/logoqery.png" alt="Edit Profile" width={40} height={40} className='rounded-lg' />
@@ -123,7 +123,7 @@ const Profile = () => {
                                         </button>
                                         <a href='https://qery.me/mas'>¿Qué es Query?</a>
                                     </div>
-                                )}
+                                )} */}
                             </div>
                         </div>
                         {/* Sección de las citas del usuario */}
